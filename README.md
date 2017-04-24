1ba04d2be5144c5e52a1c24b456a6b174655135d
davidam <d.alvarez.melis@gmail.com>
2017-04-25 01:36:31 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # drnn
 Doubly-recurrent neural networks
+
+
+### Dependencies
+
+
+#### Software
+
+* Torch7 (`luarocks install ...`)
+* penlight
+* nn
+* nngraph
+* optim
+* rnn
+* OpenNMT (only needed for MT task)
+
+
+#### Data
+
+Only data for synthetic task is provided. MT and IFTTT have to be downloaded separately.
+
+### Basic Usage
+
+./demo_vanilla.sh [TASK] (where task is one of synth, BABYMT, IFTTT)
+
+Trains, evaluates and saves predictions.
--- train_vanilla.sh
+++ train_vanilla.sh
@@ -2,40 +2,27 @@
 
 if [ "$1" == "synth" ]; then
 th main.lua -dataset synth -seq_len 20 -encoder LSTM -decoder tree,GRU,DTP \
-            -batch 20 -epochs 5 -prediction_folds 'dev,test' -clip 5 -reg 0.0001 \
-            -learning_rate 0.05 -memdim 50 -prediction -prediction_folds 'dev,test' \
+            -batch 20 -epochs 20 -prediction_folds 'dev,test' -clip 5 -reg 0.0001 \
+            -learning_rate 0.05 -memdim 150 -prediction -prediction_folds 'dev,test' \
             -dev_loss_every 1 -dev_pred_every 1 -select_criterion 'prediction' \
-            -cropv 20000 -noprompt -debug 0 -embedding rand -maxexamples 50 -shuffle
-            #-append_results -noprompt -maxexamples 10 -embedding rand
+            -cropv 20000 -noprompt -debug 0 -embedding rand -shuffle
 elif [ "$1" == "synth_STP" ]; then
 th main.lua -dataset synth -seq_len 20 -encoder LSTM -decoder tree,GRU,STP \
             -batch 20 -epochs 20 -prediction_folds 'dev,test' -clip 5 -reg 0.0001 \
-            -learning_rate 0.05 -memdim 50 -prediction -prediction_folds 'dev,test' \
+            -learning_rate 0.05 -memdim 150 -prediction -prediction_folds 'dev,test' \
             -dev_loss_every 1 -dev_pred_every 1 -select_criterion 'prediction' \
-            -cropv 20000 -noprompt -embedding rand -maxexamples 50 -debug 1
-            #-append_results -noprompt -maxexamples 10 -embedding rand
+            -cropv 20000 -noprompt
 elif [ "$1" == "IFTTT" ]; then
   th main.lua -dataset IFTTT -seq_len 15 -encoder LSTM -decoder tree,GRU,DTP \
-              -batch 25 -epochs 5 -prediction_folds 'dev,test' -clip 5 -reg 0.0001 \
-              -learning_rate 0.06 -memdim 50 -prediction -prediction_folds 'dev,test' \
+              -batch 25 -epochs 50 -prediction_folds 'dev,test' -clip 5 -reg 0.0001 \
+              -learning_rate 0.06 -memdim 100 -prediction -prediction_folds 'dev,test' \
               -dev_loss_every 1 -dev_pred_every 1 -select_criterion 'prediction' \
-              -cropv 20000 -noprompt -maxexamples 1000
-              #-append_results -noprompt -maxexamples 10 -embedding rand
-elif [ "$1" == "MT" ]; then
-  th main.lua -source fr -target en -encoder LSTM -decoder tree,GRU,DTP \
-              -batch 10 -epochs 20 -prediction_folds 'dev,test' -clip 5 -reg 0.0001 \
-              -learning_rate 0.05 -memdim 50 -prediction -prediction_folds 'dev,test' \
-              -dev_loss_every 1 -dev_pred_every 1 -select_criterion 'prediction' \
-              -cropv 50000 -maxexamples 1000 -noprompt -debug 0 -test_generation \
-              -embedding rand -debug 2
-              #-append_results -noprompt -embedding rand
+              -cropv 20000 -noprompt
 elif [ "$1" == "BABYMT" ]; then
   th main.lua -source fr -target en -encoder LSTM -decoder tree,GRU,DTP \
               -batch 25 -epochs 20 -prediction_folds 'dev,test' -clip 5 -reg 0.0001 \
               -learning_rate 0.01 -memdim 150 -prediction -prediction_folds 'dev,test' \
               -dev_loss_every 1 -dev_pred_every 1 -select_criterion 'prediction' \
               -cropv 20000 -debug 0 -test_generation -lr -seq_len 20 \
-              -nosave  -maxexamples 50 #-embedding rand
-              #-embedding rand -maxexamples 1000
-              #-append_results -noprompt
+              -nosave  -maxexamples 50
 fi
